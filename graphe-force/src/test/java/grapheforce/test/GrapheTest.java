9a5cdb1591c9dd47d1f0d46f2414cd437692a8ef
Adama Karim AYITE <karim.ayite@gmail.com>
2014-11-26 17:52:43 +0100

    Ajout du test doit_verifier_que_le_graphe_est_non_oriente

--- graphe-force/src/test/java/grapheforce/test/GrapheTest.java
+++ graphe-force/src/test/java/grapheforce/test/GrapheTest.java
@@ -6,19 +6,36 @@ import java.util.Random;
 
 import grapheforce.Graphe;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GrapheTest {
 
-	@Test
-	public void doit_verifier_que_le_graphe_ne_contient_pas_de_boucle() {
-		Graphe graphe = new Graphe(new Random().nextInt(11)+2, 0.02);
-		for (int i=0;i<graphe.getNbSommets();i++)
+	private Graphe grapheGenere;
+	
+	@Before
+	public void initialiser(){		
+		grapheGenere = new Graphe(new Random().nextInt(11)+2, 0.02);
+	}
+	
+	@Test public void doit_verifier_que_le_graphe_est_sans_boucle() {
+		for (int i=0;i<grapheGenere.getNbSommets();i++)
 		{
-			if (graphe.getMAdjacence()[i][i]!=0){
+			if (grapheGenere.getMAdjacence()[i][i]!=0){
 				fail("Présence d'une boucle");
 			}
 		}
 	}
 
+	@Test public void doit_verifier_que_le_graphe_est_non_oriente(){
+		for (int i=0;i<grapheGenere.getNbSommets();i++)
+		{
+			for (int j=0;j<grapheGenere.getNbSommets();j++)
+			{
+				if (grapheGenere.getMAdjacence()[i][j]!=0 && grapheGenere.getMAdjacence()[j][i]==0){
+					fail("Présence d'une arête orientée");
+				}
+			}
+		}
+	}
 }
